9a3a2e3377d669c1671659033f32dd2e3d91e46f
Mohamed Ibrahim <mibrahim@mibrahim.net>
2024-02-20 21:38:30 -0500

    Initial set of files

---
+++ libfatpq/autogen.sh
@@ -0,0 +1,11 @@
+#! /bin/sh
+
+case `uname` in Darwin*) glibtoolize --copy ;;
+    *) libtoolize --copy ;; esac
+
+aclocal -I m4 --install
+autoheader
+autoconf
+
+automake --foreign --add-missing --force-missing --copy
+
